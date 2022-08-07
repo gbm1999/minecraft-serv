252a8e1d8d5891e0aa0cfcf7bdc9672a02d934c3
gbm1999 <81623612+gbm1999@users.noreply.github.com>
2022-08-07 14:55:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ time.sleep(5)
 print("**script**: starting server")
 os.system('nohup java -Xmx600m -jar server.jar > nohup.out &')
 
-while open('nohup.out', 'r').read().find('Done') <= -1:
+while True:
     time.sleep(10)
+    result = open('nohup.out', 'r').read().find('Done')
+    if result > -1:
+        break
         
 print("**script**: server has started!")
 time.sleep(10)
