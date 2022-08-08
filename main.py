8bfd22095b067f02d9fad0aa3c9d59c4ec16962b
gbm1999 <81623612+gbm1999@users.noreply.github.com>
2022-08-08 11:20:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os, time, subprocess, signal
 os.system('heroku-php-apache2 &')
 print("**script**: starting web")
 time.sleep(10)
-os.system('nohup node --max-old-space-size=460 fetchworld.js > js.out & ')
+os.system('nohup node --max-old-space-size=600 fetchworld.js > js.out & ')
 print("**script**: fetching world...")
 time.sleep(2)
 
